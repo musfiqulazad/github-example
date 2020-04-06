30fec565996e8ce5f0399c9fca9d468abbf0d250
Musfiqul Azad <azad.musfiqul@gmail.com>
2020-04-07 01:49:29 +0600

    adds a python script that prints a massage greeting Github

---
+++ capstone_project.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Create a new repository on your Github account and name it Coursera_Capstone. I have prepared a guide to walk you through the process of creating a repository and setting it up. For Mac users, click here. For Windows users, click here.
+# 
+# Now, start a Jupyter Notebook using any platform that you are comfortable with and do the following:
+# 
+# 
+# 1) Import the pandas library as pd.
+# 2) Import the Numpy library as np.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# Print the following the statement: Hello Capstone Project Course!
+
+# In[ ]:
+
+
+print ("Hello Capstone Project Course!")
+
